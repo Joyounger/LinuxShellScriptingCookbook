9b5dba411edf583d29c03c03791df17cf027f426
Joyounger <942510346@qq.com>
2018-09-16 10:36:06 +0800

    ch9 src
    Signed-off-by: Joyounger <942510346@qq.com>

---
+++ ch9/create_db.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+# 创建myseq数据库和数据表
+USER="user"
+PASS="user"
+mysql -u $USER -p $PASS <<EOF 2 > dev/null
+CREATE DATABASE student;
+EOF
+
+[ $? -eq 0 ] && echo Created DB || echo DB already exist
+mysql -u $USER -p$PASS students << EOF 2> /dev/null
+CREATE TABLE students(
+id int,
+make 
+	)
